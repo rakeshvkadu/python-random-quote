6d9d0308721babcd2ca70c29b5dc87a966efc531
rakeshvkadu <rakeshvkadu@gmail.com>
2020-05-21 04:47:17 +0000

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,16 @@
-import random
+#import random
 def primary():
  #  print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
+  f = open("quotes.txt","a")
+  f.write("I am learnig Paython \n")
+  f.close
+  f=open("quotes.txt")
   quotes = f.readlines()
   f.close()
   last=len(quotes)-1
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  #rnd = random.randint(0, last)
+  print(quotes[0],quotes[last])
 
 if __name__== "__main__":
   primary()
